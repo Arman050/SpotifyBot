da529160c2373ded9e6192abe8b1f87684432911
Arman Avetissian <arman.avetissian@edu.technobel.be>
2024-10-14 22:10:59 +0200

    useragent

--- server.js
+++ server.js
@@ -295,7 +295,7 @@ const addCustomCursor = async (page) => {
 
         const page = await browser.newPage();
 
-        // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36');
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36');
 
         // Ajouter le curseur personnalisé
         await addCustomCursor(page);
